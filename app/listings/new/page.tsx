99446ab91c2ec095fcfd9e8c4eb00d89865c9a00
khalidnbg <khalidnabgaoui4@gmail.com>
2026-09-11 00:59:44 +0100

    fix: correct responsive class names for layout in NewListing component

--- app/listings/new/page.tsx
+++ app/listings/new/page.tsx
@@ -2,7 +2,7 @@ import ListingForm from "@/components/ListingForm"
 
 const NewListing = () => {
 	return (
-		<main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+		<main className="lg:w-1/3 md:w-2/3 items-center justify-center">
 
 			<article className="w-full gap-4 flex flex-col">
 				<h1>Listing Builder</h1>
